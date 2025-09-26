2548dced34ad7f851f727075792ecd26b3cb752c
arsyadpcr <arsyad24si@mahasiswa.pcr.ac.id>
2025-09-26 10:31:10 +0700

    Latihan Passing Data dan Git.

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DateTime;
 
 class PegawaiController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $data['name'] = "Isan";
+
+        $tanggal_lahir = new DateTime("2005-05-12");
+        $hari_ini = new DateTime();
+        $data['my_age'] = $hari_ini->diff($tanggal_lahir)->y;
+
+        $data['hobbies'] = [
+            "Badminton",
+            "Membaca",
+            "Gaming",
+            "Sepak bola",
+            "Traveling"
+        ];
+
+        $data['tgl_harus_wisuda'] = "2026-07-01";
+
+
+        $tgl_wisuda = new DateTime($data['tgl_harus_wisuda']);
+        $selisih = $hari_ini->diff($tgl_wisuda);
+        $data['time_to_study_left'] = $selisih->days;
+
+
+        $data['current_semester'] = 2;
+
+
+        $data['message'] = $data['current_semester'] < 3
+            ? "Masih Awal, Kejar TAK"
+            : "Jangan main-main, kurang-kurangi main game!";
+
+        $data['future_goal'] = "Menjadi orang yang bermanfaat bagi orang lain";
+        return view('pegawai-view', $data);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PegawaiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,11 +17,11 @@ Route::get('/mahasiswa', function () {
 });
 
 Route::get('/nama/{param1}', function ($param1) {
-    return 'Nama saya: '.$param1;
+    return 'Nama saya: ' . $param1;
 });
 
 Route::get('/nim/{param1?}', function ($param1 = '') {
-    return 'NIM saya: '.$param1;
+    return 'NIM saya: ' . $param1;
 });
 
 Route::get('/mahasiswa', function () {
@@ -29,3 +31,7 @@ Route::get('/mahasiswa', function () {
 Route::get('/about', function () {
     return view('halaman-about');
 });
+
+Route::get('/home', [HomeController::class, 'index']);
+
+Route::get('/pegawai', [PegawaiController::class, 'index']);
