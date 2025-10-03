988d81f96edd53a65250449e4ac252b489b4fdc1
arsyad0098 <arsyad24si@mahasiswa.pcr.ac.id>
2025-10-03 10:10:14 +0700

    penambahan form pada home

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -35,3 +36,5 @@ Route::get('/about', function () {
 Route::get('/home', [HomeController::class, 'index']);
 
 Route::get('/pegawai', [PegawaiController::class, 'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])->name('question.store');
